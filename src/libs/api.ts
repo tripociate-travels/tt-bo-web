4259eddcdc0867f656e59ba71034b4bd13095765
Maruf Hossain <mhtamun@gmail.com>
2025-02-23 23:23:29 +0600

    Refactor

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -42,7 +42,7 @@ instance.interceptors.response.use(
         // Any status code that lie within the range of 2xx cause this function to trigger
         // Do something with response data
 
-        console.debug('response -> data', response.data);
+        // console.debug('response -> data', response.data);
 
         return response.data;
     },
